bdc7493c38b0fc2d1a9e55f6735609b0f4cdb052
Thomas Peruch <peruchthomas@gmail.com>
2026-03-02 20:11:51 -0300

    solution

--- src/main/java/teste/Main.java
+++ src/main/java/teste/Main.java
@@ -1,12 +1,13 @@
 package teste;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Integer[] ids = {10,22,35,99,88};
-        List<Integer> idsList = Arrays.asList(ids);
-        idsList.add(7); //throw UnsuportedOperationException
+        List<Integer> idsList = new ArrayList<>(Arrays.asList(ids));
+        idsList.add(7);
     }
 }
